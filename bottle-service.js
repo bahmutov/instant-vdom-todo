0b277b1eaaa87b61576c76882616ca628f8e030e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-23 15:20:34 -0500

    Updates

---
+++ bottle-service.js
@@ -0,0 +1,134 @@
+'use strict'
+
+/*
+  This is ServiceWorker code
+*/
+/* global self, Response, Promise, location, fetch, caches */
+var myName = 'bottle-service'
+console.log(myName, 'startup')
+
+// Poor man's async "localStorage" on top of Cache
+// https://developer.mozilla.org/en-US/docs/Web/API/Cache
+// TODO factor into separate module
+function dataStore () {
+  return caches.open(myName + '-v1')
+    .then(function (cache) {
+      return {
+        write: function (key, data) {
+          return cache.put(key, new Response(JSON.stringify(data)))
+        },
+        read: function (key) {
+          return cache.match(key)
+            .then(function (res) {
+              return res &&
+                res.text().then(JSON.parse)
+            })
+        }
+      }
+    })
+}
+
+self.addEventListener('install', function (event) {
+  console.log(myName, 'installed')
+})
+
+self.addEventListener('activate', function () {
+  console.log(myName, 'activated')
+})
+
+function isIndexPageRequest (event) {
+  return event &&
+    event.request &&
+    event.request.url === location.origin + '/'
+}
+
+self.addEventListener('fetch', function (event) {
+  if (!isIndexPageRequest(event)) {
+    return fetch(event.request)
+  }
+
+  console.log(myName, 'fetching index page', event.request.url)
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        return dataStore()
+          .then(function (store) {
+            return store.read('contents')
+          })
+          .then(function (contents) {
+            if (contents && contents.html && contents.id) {
+              console.log('fetched latest', response.url, 'need to update')
+              console.log('element "%s" with html "%s" ...',
+                contents.id, contents.html.substr(0, 15))
+
+              var copy = response.clone()
+              return copy.text().then(function (pageHtml) {
+                console.log('inserting our html')
+                // HACK using id in the CLOSING TAG to find fragment
+                var toReplaceStart = '<div id="' + contents.id + '">'
+                var toReplaceFinish = '</div id="' + contents.id + '">'
+                var startIndex = pageHtml.indexOf(toReplaceStart)
+                var finishIndex = pageHtml.indexOf(toReplaceFinish)
+                if (startIndex !== -1 && finishIndex > startIndex) {
+                  console.log('found fragment')
+                  pageHtml = pageHtml.substr(0, startIndex + toReplaceStart.length) +
+                    '\n' + contents.html + '\n' +
+                    pageHtml.substr(finishIndex)
+                }
+
+                // console.log('page html')
+                // console.log(pageHtml)
+
+                var responseOptions = {
+                  status: 200,
+                  headers: {
+                    'Content-Type': 'text/html charset=UTF-8'
+                  }
+                }
+                return new Response(pageHtml, responseOptions)
+              })
+            } else {
+              return response
+            }
+          }, function notFound () {
+            return response
+          })
+      })
+  )
+})
+
+// use window.navigator.serviceWorker.controller.postMessage('hi')
+// to communicate with this service worker
+self.onmessage = function onMessage (event) {
+  console.log('message to bottle-service worker cmd', event.data && event.data.cmd)
+
+  // TODO how to use application name?
+
+  dataStore().then(function (store) {
+    switch (event.data.cmd) {
+      case 'print': {
+        return store.read('contents')
+          .then(function (res) {
+            console.log('bottle service has contents')
+            console.log(res)
+          })
+      }
+      case 'clear': {
+        console.log('clearing the bottle')
+        return store.write('contents', {})
+      }
+      case 'refill': {
+        return store.write('contents', {
+          html: event.data.html,
+          id: event.data.id
+        }).then(function () {
+          console.log('saved new html for id', event.data.id)
+        })
+      }
+      default: {
+        console.error(myName, 'unknown command', event.data)
+      }
+    }
+  })
+}
---
+++ bottle.js
@@ -0,0 +1,103 @@
+!(function startBottleService (root) {
+  'use strict'
+
+  if (!root.navigator) {
+    console.error('Missing navigator')
+    return
+  }
+
+  if (!root.navigator.serviceWorker) {
+    console.error('Sorry, not ServiceWorker feature, maybe enable it?')
+    console.error('http://jakearchibald.com/2014/using-serviceworker-today/')
+    return
+  }
+
+  // TODO package lazy-ass and check-more-types using webpack
+
+  function toString (x) {
+    return typeof x === 'string' ? x : JSON.stringify(x)
+  }
+
+  function la (condition) {
+    if (!condition) {
+      var args = Array.prototype.slice.call(arguments, 1)
+        .map(toString)
+      throw new Error(args.join(' '))
+    }
+  }
+
+  function isFunction (f) {
+    return typeof f === 'function'
+  }
+
+  function getCurrentScriptFolder () {
+    var scriptEls = document.getElementsByTagName('script')
+    var thisScriptEl = scriptEls[scriptEls.length - 1]
+    var scriptPath = thisScriptEl.src
+    return scriptPath.substr(0, scriptPath.lastIndexOf('/') + 1)
+  }
+
+  var serviceScriptUrl = getCurrentScriptFolder() + 'bottle-service.js'
+  var scope = '/'
+
+  var send = function mockSend () {
+    console.error('Bottle service not initialized yet')
+  }
+
+  function registeredWorker (registration) {
+    la(registration, 'missing service worker registration')
+    la(registration.active, 'missing active service worker')
+    la(isFunction(registration.active.postMessage),
+      'expected function postMessage to communicate with service worker')
+
+    send = registration.active.postMessage.bind(registration.active)
+    var info = '\nbottle-service - .\n' +
+      'I have a valid service-turtle, use `bottleService` object to update cached page'
+    console.log(info)
+
+    registration.active.onmessage = function messageFromServiceWorker (e) {
+      console.log('received message from the service worker', e)
+    }
+  }
+
+  function onError (err) {
+    if (err.message.indexOf('missing active') !== -1) {
+      // the service worker is installed
+      window.location.reload()
+    } else {
+      console.error('bottle service error', err)
+    }
+  }
+
+  root.navigator.serviceWorker.register(serviceScriptUrl, { scope: scope })
+    .then(registeredWorker)
+    .catch(onError)
+
+  root.bottleService = {
+    refill: function refill (applicationName, id) {
+      console.log('bottle-service: html for app %s element %s', applicationName, id)
+
+      var el = document.getElementById(id)
+      la(el, 'could not find element with id', id)
+      var html = el.innerHTML
+      send({
+        cmd: 'refill',
+        html: html,
+        name: applicationName,
+        id: id
+      })
+    },
+    print: function print (applicationName) {
+      send({
+        cmd: 'print',
+        name: applicationName
+      })
+    },
+    clear: function clear (applicationName) {
+      send({
+        cmd: 'clear',
+        name: applicationName
+      })
+    }
+  }
+}(window))
